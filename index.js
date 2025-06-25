08845fb2c3b875359e91d62bf7036ad5c909bf13
nyagetiria <nyagetirianaomi@gmail.com>
2025-06-25 21:16:18 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      const body = document.querySelector("body");
+      body.innerHTML += `<p>ID: ${data.id}</p>`;
+    })
+    .catch(error => {
+      const body = document.querySelector("body");
+      body.innerHTML += `<p>Error: ${error.message}</p>`;
+    });
+}
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { submitData };
+}
+
